9ead36ee68b01055c7298d2fe5f44eadad56c0b0
Smoopi <74732940+Smoopi@users.noreply.github.com>
2025-10-10 19:36:28 +0000

    Chnaged image to a cookie, and made clickable

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
+import exampleIconUrl from "./pngtree-cartoon-cookies-png-image_6516299.png";
 import "./style.css";
 
 document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Click the cookie: <img src="${exampleIconUrl}" class="icon" id="cookie" /></p>
 `;
+
+const cookieImg = document.querySelector<HTMLImageElement>("#cookie");
+if (!cookieImg) {
+  throw new Error("Cookie image not found");
+}
+
+cookieImg.style.cursor = "pointer";
+
+cookieImg.addEventListener("click", () => {
+  console.log("Cookie clicked! (Step 1 test)");
+
+  cookieImg.style.transform = "scale(0.95)";
+  setTimeout(() => {
+    cookieImg.style.transform = "";
+  }, 100);
+});
